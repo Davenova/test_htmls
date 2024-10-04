7e5305d853079232c1d79f75f6161cabd86495bc
Amlan Biswas <dudebrothers2@gmail.com>
2024-10-04 12:10:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Get the button element
+const button = document.getElementById('myButton');
+
+// Add an event listener to the button
+button.addEventListener('click', () => {
+  if (button.textContent.trim() === 'Check') {
+    // Open the YouTube link and change the button text to "Claim"
+    button.textContent = 'Claim';
+    button.classList.remove('bg-blue-500', 'hover:bg-blue-700');
+    button.classList.add('bg-white', 'text-green-500');
+    window.open('hts://www.ytrtro.com/', '_blank'); // Replace with your YouTube link
+  } else {
+    // On subsequent clicks, only change the button color
+    button.classList.add('bg-white', 'text-green-500');
+  }
+});
